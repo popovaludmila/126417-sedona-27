02622afacaecc939bf9383fcc76065461010efbc
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2020-03-03 21:30:03 +0300

    работа с формой

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
 var modal = document.querySelector('.modal-search');
 var open = document.querySelector('.btn-search');
-console.log(modal);
+var form = modal.querySelector('.search-form');
+var calendar = form.querySelectorAll('.btn-calend');
 
-open.addEventListener('click', function(evt) {
+var number = form.querySelector('.number');
+var date = form.querySelector('.date');
+var input = form.querySelectorAll('.input');
+
+open.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modal.classList.toggle('visually-hidden');
+  if (!modal.classList.contains('visually-hidden')) {
+    modal.classList.add('visually-hidden');
+  } else {
+    modal.classList.remove('visually-hidden');
+  }
 });
+
+form.addEventListener('submit', function (evt) {
+  if (input.value === '') {
+    evt.preventDefault();
+  } else {
+    form.submit();
+  }
+});
+
+calendar.addEventListener('click', function(evt) {
+
+});
+
+
+number.addEventListener('click', function (evt) {
+  if (evt.target.classList.contains('btn-plus')) {
+    ++evt.target.parentElement.querySelector('input').value;
+  } else if (evt.target.classList.contains('btn-minus')) {
+    --evt.target.parentElement.querySelector('input').value;
+  }
+})
