bded87ae96733d155d4d62591474d5a197a61f5d
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2020-03-04 21:26:19 +0300

    фокусировка на поле ввода

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
 var modal = document.querySelector('.modal-search');
-var open = document.querySelector('.btn-search');
+var buttonOpen = document.querySelector('.btn-search');
 var form = modal.querySelector('.search-form');
+var datein = form.querySelector('#datein');
+var number = form.querySelector('.number');
+
+var input = form.querySelectorAll('.input');
 var calendar = form.querySelectorAll('.btn-calend');
 
-var number = form.querySelector('.number');
-var date = form.querySelector('.date');
-var input = form.querySelectorAll('.input');
-
-open.addEventListener('click', function (evt) {
+datein.focus();
+buttonOpen.addEventListener('click', function (evt) {
   evt.preventDefault();
   if (!modal.classList.contains('visually-hidden')) {
+    datein.focus();
     modal.classList.add('visually-hidden');
+    modal.classList.remove('floatUp');
   } else {
     modal.classList.remove('visually-hidden');
+    datein.focus();
+    modal.classList.add('floatUp');
   }
+
 });
 
 form.addEventListener('submit', function (evt) {
-  if (input.value === '') {
+  if (!input.value) {
     evt.preventDefault();
   } else {
     form.submit();
   }
 });
-
-calendar.addEventListener('click', function(evt) {
-
-});
-
 
 number.addEventListener('click', function (evt) {
   if (evt.target.classList.contains('btn-plus')) {
